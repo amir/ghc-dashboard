dca1714af46ca10ec798e3c2bf0ca8c69ed8a85a
Amir Mohammad Saeid <amirsaied@gmail.com>
2017-01-26 08:55:32 +0000

    Add signature validation

--- src/Main.hs
+++ src/Main.hs
@@ -10,6 +10,9 @@ import Text.Read (readMaybe)
 import Data.Maybe (fromMaybe)
 import qualified Data.Text as T
 import qualified Data.Text.Lazy as TL
+import qualified Data.ByteString.Lazy as BL
+import qualified Data.ByteString as B
+import qualified Crypto.Hash as Hash
 
 import GitHub.Types
 
@@ -19,12 +22,21 @@ readEvent e = readMaybe (TL.unpack $ TL.toTitle e)
 act :: Object -> GithubEventType -> Maybe TL.Text
 act payload event = Just $ TL.pack (show event)
 
+checkSignature :: TL.Text -> BL.ByteString -> Bool
+checkSignature signature body =
+  signature == digest
+  where
+    digest = TL.pack $ show $ sha1 $ BL.toStrict body
+
+    sha1 :: B.ByteString -> Hash.Digest Hash.SHA1
+    sha1  = Hash.hash
+
 app :: ScottyM ()
-app = 
+app = do
   post "/github-webhooks" $ do
-    b  <- jsonData
-    e  <- header "X-Github-Event"
-    text $ fromMaybe (TL.pack "") ((e >>= readEvent) >>= act b)
+    b  <- body
+    s  <- header "X-Hub-Signature"
+    text $ TL.pack $ show $ checkSignature (fromMaybe TL.empty s) b
 
 main :: IO ()
 main = scotty 8080 app
