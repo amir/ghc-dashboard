450f29bc2c5e6139eaed57e78706ad0fe9e7fd90
Amir Mohammad Saeid <amirsaied@gmail.com>
2017-01-27 20:56:02 +0000

    Use github package

--- src/GitHub/Types.hs
+++
@@ -1,6 +0,0 @@
-module GitHub.Types where
-
-data GithubEventType = Ping
-                     | Issues
-                     | Unknown
-                     deriving (Read, Show, Eq)
--- src/Main.hs
+++ src/Main.hs
@@ -1,42 +1,41 @@
-{-# LANGUAGE DeriveGeneric     #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Main where
 
 import Web.Scotty
-import Data.Aeson hiding (json)
-import GHC.Generics
-import Text.Read (readMaybe)
-import Data.Maybe (fromMaybe)
-import qualified Data.Text as T
+import Data.Aeson
+import GitHub.Data.Webhooks
+import GitHub.Data.Webhooks.Validate
+import Data.Monoid ((<>))
+
+import Network.HTTP.Types.Status
 import qualified Data.Text.Lazy as TL
 import qualified Data.ByteString.Lazy as BL
-import qualified Data.ByteString as B
-import qualified Crypto.Hash as Hash
+import qualified Data.ByteString.Builder as B
+import qualified Data.Text.Lazy.Encoding as E
 
-import GitHub.Types
-
-readEvent :: TL.Text -> Maybe GithubEventType
-readEvent e = readMaybe (TL.unpack $ TL.toTitle e)
-
-act :: Object -> GithubEventType -> Maybe TL.Text
-act payload event = Just $ TL.pack (show event)
-
-checkSignature :: TL.Text -> BL.ByteString -> Bool
-checkSignature signature body =
-  signature == digest
-  where
-    digest = TL.pack $ show $ sha1 $ BL.toStrict body
-
-    sha1 :: B.ByteString -> Hash.Digest Hash.SHA1
-    sha1  = Hash.hash
+act :: BL.ByteString -> RepoWebhookEvent -> TL.Text
+act _ event = TL.pack $ show event
 
 app :: ScottyM ()
 app = do
   post "/github-webhooks" $ do
-    b  <- body
-    s  <- header "X-Hub-Signature"
-    text $ TL.pack $ show $ checkSignature (fromMaybe TL.empty s) b
+    b <- body
+    s <- header "X-Hub-Signature"
+    e <- header "X-Github-Event"
+    if isValid s b
+      then case event e of
+        Just e' -> text $ act b e'
+        Nothing -> status badRequest400
+      else
+        status badRequest400
+    where
+      event :: Maybe TL.Text -> Maybe RepoWebhookEvent
+      event z = fmap E.encodeUtf8 z >>= \x ->
+        decode $ B.toLazyByteString $ q <> B.lazyByteString x <> q
+      q = B.charUtf8 '\"'
+      isValid s b = isValidPayload secret (fmap TL.toStrict s) (BL.toStrict b)
+      secret = "Secret"
 
 main :: IO ()
 main = scotty 8080 app
