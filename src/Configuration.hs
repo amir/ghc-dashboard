71e1523c91e4cd6dde9b5d16e4d80af7275e7534
Amir M. Saeid <amir@glgdgt.com>
2017-03-05 14:08:44 +0000

    Add database connection pool to config

--- src/Configuration.hs
+++ src/Configuration.hs
@@ -7,20 +7,37 @@ import qualified Data.Text as T
 import qualified Data.ByteString as B
 import Control.Monad.IO.Class (MonadIO, liftIO)
 import Control.Monad.Reader (MonadReader, ReaderT, runReaderT, asks, lift)
+import Control.Monad.Logger (runStdoutLoggingT)
+import Control.Monad.Trans.Class (MonadTrans, lift)
+
+import Database.Persist as DB
+import Database.Persist.Sqlite as DB
 
 data Config = Config {
     githubWebhooksSecret :: T.Text
   , githubUsername :: B.ByteString
   , githubPassword :: B.ByteString
-  } deriving (Eq, Show, Read)
+  , pool           :: DB.ConnectionPool
+  } deriving Show
 
 newtype ConfigM a = ConfigM {
   runConfigM :: ReaderT Config IO a
   } deriving (Applicative, Functor, Monad, MonadIO, MonadReader Config)
 
+getPool :: IO DB.ConnectionPool
+getPool = runStdoutLoggingT (DB.createSqlitePool ":memory:" 1)
+
+runDB :: (MonadTrans t, MonadIO (t ConfigM)) => DB.SqlPersistT IO a -> t ConfigM a
+runDB q = do
+  p <- lift (asks pool)
+  liftIO (DB.runSqlPool q p)
+
 getConfig :: IO Config
-getConfig = return Config {
-    githubWebhooksSecret = "secret"
-  , githubUsername = "username"
-  , githubPassword = "password"
+getConfig = do
+  p <- getPool
+  return Config {
+      pool                 = p
+    , githubUsername       = "username"
+    , githubPassword       = "password"
+    , githubWebhooksSecret = "secret"
   }
